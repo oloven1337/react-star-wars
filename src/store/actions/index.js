5221b20a8ac1ec5aee958c85630c38576d7a1cfd
Ibragim <oloven1337@gmail.com>
2021-08-15 13:36:38 +0300

    adding redux

---
+++ src/store/actions/index.js
@@ -0,0 +1,11 @@
+import {AAD_PERSON_TO_FAVORITE, REMOVE_PERSON_FROM_FAVORITE} from "../constans/actionTypes";
+
+export const setPersonToFavorite = () => ({
+    type: AAD_PERSON_TO_FAVORITE,
+    payload: ''
+})
+
+export const removePersonFromFavorite = () => ({
+    type: REMOVE_PERSON_FROM_FAVORITE,
+    payload: ''
+})
